8154c53bd2b0de472d4357dcff67d7e578dfb947
yshong <ysh975@gmail.com>
2025-04-18 12:56:18 +0900

    전부 commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
