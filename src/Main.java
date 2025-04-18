7b382ec06c9e0036055e7dd3ad95a4ea44b62da7
yshong <ysh975@gmail.com>
2025-04-18 21:53:49 +0900

    바뀐거 확인

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public String toString() {
         return super.toString();
     }
-
+// 테스트를 위한 설명
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
